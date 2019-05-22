4fb34a31c24ce7cba9fc0b26e066715c8b64303f
Unknown <shekwancheung0528@gmail.com>
2019-05-22 23:14:01 +0800

    v1.2

--- src/main/java/me/shedaniel/lightoverlay/LightOverlay.java
+++ src/main/java/me/shedaniel/lightoverlay/LightOverlay.java
@@ -13,13 +13,15 @@ import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.Tessellator;
 import net.minecraft.client.render.VertexFormats;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityCategory;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.BoundingBox;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.LightType;
-import net.minecraft.world.SpawnHelper;
 import net.minecraft.world.World;
 
 import java.awt.*;
@@ -28,18 +30,21 @@ public class LightOverlay implements ClientModInitializer {
     
     private static final String KEYBIND_CATEGORY = "key.lightoverlay.category";
     private static final Identifier ENABLE_OVERLAY_KEYBIND = new Identifier("lightoverlay", "enable_overlay");
-    private static final BoundingBox TEST_BOX = new BoundingBox(0.6D / 2D, 0, 0.6D / 2D, 1D - 0.6D / 2D, 1D, 1D - 0.6D / 2D);
-    private static FabricKeyBinding enableOverlay;
+    private static final Identifier INCREASE_REACH_KEYBIND = new Identifier("lightoverlay", "increase_reach");
+    private static final Identifier DECREASE_REACH_KEYBIND = new Identifier("lightoverlay", "decrease_reach");
+    private static FabricKeyBinding enableOverlay, increaseReach, decreaseReach;
     private static boolean enabled = false;
     private static int reach = 12;
+    private static EntityType<Entity> testingEntityType;
     
     public static CrossType getCrossType(BlockPos pos, World world, PlayerEntity playerEntity) {
         BlockState blockBelowState = world.getBlockState(pos.down());
+        BlockState blockState = world.getBlockState(pos);
+        if (blockBelowState.isAir() || !blockState.isAir())
+            return CrossType.NONE;
         if (blockBelowState.getBlock() == Blocks.BEDROCK || blockBelowState.getBlock() == Blocks.BARRIER)
             return CrossType.NONE;
-        if ((!blockBelowState.getMaterial().blocksLight() && blockBelowState.isTranslucent(world, pos.down())) || !SpawnHelper.isClearForSpawn(world, pos, world.getBlockState(pos), world.getFluidState(pos)))
-            return CrossType.NONE;
-        if (blockBelowState.isAir() || !world.getBlockState(pos).isAir() || !blockBelowState.hasSolidTopSurface(world, pos, playerEntity) || !world.getFluidState(pos.down()).isEmpty())
+        if (!blockBelowState.allowsSpawning(world, pos.down(), testingEntityType))
             return CrossType.NONE;
         if (world.getLightLevel(LightType.BLOCK, pos) >= 8)
             return CrossType.NONE;
@@ -71,12 +76,25 @@ public class LightOverlay implements ClientModInitializer {
     
     @Override
     public void onInitializeClient() {
+        testingEntityType = EntityType.Builder.create(EntityCategory.MONSTER).setSize(0f, 0f).disableSaving().build(null);
         MinecraftClient client = MinecraftClient.getInstance();
         KeyBindingRegistryImpl.INSTANCE.addCategory(KEYBIND_CATEGORY);
         KeyBindingRegistryImpl.INSTANCE.register(enableOverlay = FabricKeyBinding.Builder.create(ENABLE_OVERLAY_KEYBIND, InputUtil.Type.KEYSYM, 296, KEYBIND_CATEGORY).build());
+        KeyBindingRegistryImpl.INSTANCE.register(increaseReach = FabricKeyBinding.Builder.create(INCREASE_REACH_KEYBIND, InputUtil.Type.KEYSYM, -1, KEYBIND_CATEGORY).build());
+        KeyBindingRegistryImpl.INSTANCE.register(decreaseReach = FabricKeyBinding.Builder.create(DECREASE_REACH_KEYBIND, InputUtil.Type.KEYSYM, -1, KEYBIND_CATEGORY).build());
         ClothClientHooks.HANDLE_INPUT.register(minecraftClient -> {
             while (enableOverlay.wasPressed())
                 enabled = !enabled;
+            while (increaseReach.wasPressed()) {
+                if (reach < 50)
+                    reach++;
+                client.player.addChatMessage(new TranslatableComponent("text.lightoverlay.current_reach", reach), false);
+            }
+            while (decreaseReach.wasPressed()) {
+                if (reach > 1)
+                    reach--;
+                client.player.addChatMessage(new TranslatableComponent("text.lightoverlay.current_reach", reach), false);
+            }
         });
         ClothClientHooks.DEBUG_RENDER_PRE.register(() -> {
             if (LightOverlay.enabled) {
