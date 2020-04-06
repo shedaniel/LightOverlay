64132f7f5418a8ef08293a382a143576be33a4d9
shedaniel <daniel@shedaniel.me>
2020-04-06 21:16:45 +0800

    20w14a
    Signed-off-by: shedaniel <daniel@shedaniel.me>

--- src/main/java/me/shedaniel/lightoverlay/fabric/LOModMenuEntry.java
+++ src/main/java/me/shedaniel/lightoverlay/fabric/LOModMenuEntry.java
@@ -6,11 +6,7 @@ import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import me.shedaniel.clothconfig2.api.ConfigCategory;
 import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.resource.language.I18n;
 import net.minecraft.util.math.MathHelper;
-
-import java.util.Locale;
-import java.util.Optional;
 
 public class LOModMenuEntry implements ModMenuApi {
     @Override
@@ -32,18 +28,8 @@ public class LOModMenuEntry implements ModMenuApi {
         general.addEntry(eb.startIntSlider("config.lightoverlay.crossLevel", LightOverlay.crossLevel, 0, 15).setDefaultValue(7).setTextGetter(integer -> "Cross Level: " + integer).setSaveConsumer(integer -> LightOverlay.crossLevel = integer).build());
         general.addEntry(eb.startBooleanToggle("config.lightoverlay.showNumber", LightOverlay.showNumber).setDefaultValue(false).setSaveConsumer(bool -> LightOverlay.showNumber = bool).build());
         general.addEntry(eb.startIntSlider("config.lightoverlay.lineWidth", MathHelper.floor(LightOverlay.lineWidth * 100), 100, 700).setDefaultValue(100).setTextGetter(integer -> "Light Width: " + LightOverlay.FORMAT.format(integer / 100d)).setSaveConsumer(integer -> LightOverlay.lineWidth = integer / 100f).build());
-        general.addEntry(eb.startStrField("config.lightoverlay.yellowColor", "#" + toStringColor(LightOverlay.yellowColor)).setDefaultValue("#FFFF00").setSaveConsumer(str -> LightOverlay.yellowColor = toIntColor(str)).setErrorSupplier(s -> {
-            if (!s.startsWith("#") || s.length() != 7 || !isInt(s.substring(1)))
-                return Optional.of(I18n.translate("config.lightoverlay.invalidColor"));
-            else
-                return Optional.empty();
-        }).build());
-        general.addEntry(eb.startStrField("config.lightoverlay.redColor", "#" + toStringColor(LightOverlay.redColor)).setDefaultValue("#FF0000").setSaveConsumer(str -> LightOverlay.redColor = toIntColor(str)).setErrorSupplier(s -> {
-            if (!s.startsWith("#") || s.length() != 7 || !isInt(s.substring(1)))
-                return Optional.of(I18n.translate("config.lightoverlay.invalidColor"));
-            else
-                return Optional.empty();
-        }).build());
+        general.addEntry(eb.startColorField("config.lightoverlay.yellowColor", LightOverlay.yellowColor).setDefaultValue(0xFFFF00).setSaveConsumer(color -> LightOverlay.yellowColor = color).build());
+        general.addEntry(eb.startColorField("config.lightoverlay.redColor", LightOverlay.redColor).setDefaultValue(0xFF0000).setSaveConsumer(color -> LightOverlay.redColor = color).build());
         
         return builder.setSavingRunnable(() -> {
             try {
@@ -53,35 +39,5 @@ public class LOModMenuEntry implements ModMenuApi {
             }
             LightOverlay.loadConfig(LightOverlay.configFile);
         }).build();
-    }
-    
-    private boolean isInt(String s) {
-        try {
-            Integer.parseInt(s, 16);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-        }
-    }
-    
-    private int toIntColor(String str) {
-        String substring = str.substring(1);
-        int r = Integer.parseInt(substring.substring(0, 2), 16);
-        int g = Integer.parseInt(substring.substring(2, 4), 16);
-        int b = Integer.parseInt(substring.substring(4, 6), 16);
-        return (r << 16) + (g << 8) + b;
-    }
-    
-    private String toStringColor(int toolColor) {
-        String r = Integer.toHexString((toolColor >> 16) & 0xFF);
-        String g = Integer.toHexString((toolColor >> 8) & 0xFF);
-        String b = Integer.toHexString(toolColor & 0xFF);
-        if (r.length() == 1)
-            r = "0" + r;
-        if (g.length() == 1)
-            g = "0" + g;
-        if (b.length() == 1)
-            b = "0" + b;
-        return (r + g + b).toUpperCase(Locale.ROOT);
     }
 }
