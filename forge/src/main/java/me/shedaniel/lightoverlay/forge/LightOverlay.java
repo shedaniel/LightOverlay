063bc9b2cb9c0fa15543c1d4c0c27e12531febf5
Philip T <me@phit.link>
2020-10-09 15:57:52 +0200

    Mark mod clientside only for networking
    https://mcforge.readthedocs.io/en/latest/concepts/sides/#writing-one-sided-mods

--- forge/src/main/java/me/shedaniel/lightoverlay/forge/LightOverlay.java
+++ forge/src/main/java/me/shedaniel/lightoverlay/forge/LightOverlay.java
@@ -2,12 +2,16 @@ package me.shedaniel.lightoverlay.forge;
 
 import net.minecraft.network.IPacket;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 
 @Mod("lightoverlay-forge")
 public class LightOverlay {
     public LightOverlay() {
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
         DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> LightOverlayClient::register);
     }
     
