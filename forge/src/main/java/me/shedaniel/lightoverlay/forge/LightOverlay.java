3300b5495da17f7f8697475324b75b03ff45bcb5
shedaniel <daniel@shedaniel.me>
2020-10-24 19:34:44 +0800

    Bump to 5.5.4
    Signed-off-by: shedaniel <daniel@shedaniel.me>

--- forge/src/main/java/me/shedaniel/lightoverlay/forge/LightOverlay.java
+++ forge/src/main/java/me/shedaniel/lightoverlay/forge/LightOverlay.java
@@ -4,6 +4,7 @@ import net.minecraft.network.IPacket;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.network.FMLNetworkConstants;
 import org.apache.commons.lang3.tuple.Pair;
