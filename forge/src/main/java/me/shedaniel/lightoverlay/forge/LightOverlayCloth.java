c01e3b196f1f73e16c507d3a60a339da6dd8cb1c
shedaniel <daniel@shedaniel.me>
2022-07-12 22:54:11 +0800

    Fix #150 and Fix #147

--- forge/src/main/java/me/shedaniel/lightoverlay/forge/LightOverlayCloth.java
+++ forge/src/main/java/me/shedaniel/lightoverlay/forge/LightOverlayCloth.java
@@ -1,12 +1,12 @@
 package me.shedaniel.lightoverlay.forge;
 
 import me.shedaniel.lightoverlay.common.ClothScreen;
-import net.minecraftforge.client.ConfigGuiHandler;
+import net.minecraftforge.client.ConfigScreenHandler;
 import net.minecraftforge.fml.ModLoadingContext;
 
 public class LightOverlayCloth {
     public static void register() {
-        ModLoadingContext.get().registerExtensionPoint(ConfigGuiHandler.ConfigGuiFactory.class, () -> new ConfigGuiHandler.ConfigGuiFactory((client, parent) -> {
+        ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> new ConfigScreenHandler.ConfigScreenFactory((client, parent) -> {
             return ClothScreen.getConfigScreenByCloth(parent);
         }));
     }
